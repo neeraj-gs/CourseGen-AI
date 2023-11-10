3ce5517e9a542c43f9e37baa511fa92e49eeea18
neeraj-gs <gsneeraj05@gmail.com>
2023-11-10 12:47:07 +0530

    Added Dropdown menu that will show user details anda  sign out button also added sign out button that signs out user

--- coursegen-ai/src/components/UserAccountNav.tsx
+++ coursegen-ai/src/components/UserAccountNav.tsx
@@ -1,8 +1,9 @@
 'use client'
-import React from 'react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from './ui/dropdown-menu'
-import { Button } from './ui/button'
 import { User } from 'next-auth'
+import { Button } from './ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { signOut } from 'next-auth/react'
+import { LogOut } from 'lucide-react'
 
 type Props = {
     user: User //user is from sessoin so we import user from next autha dn not our prisma claint
@@ -16,9 +17,18 @@ const UserAccountNav = ({user}: Props) => {
         </DropdownMenuTrigger>
         <DropdownMenuContent align='end'>
             <div className='flex items-center justify-start gap-2 p-2'>
+                <div className='flex flex-col space-y-1 leading-none'>
                 {user?.name && (<p className='font-medium'>{user.name}</p>)}
                 {user?.email && (<p className='w-[200px] truncate text-sm text-secondary-foreground'>{user.email}</p>)}
+                </div>
             </div>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className='text-red-600 cursor-pointer' onSelect={()=>{
+                signOut();
+            }} >
+                Sign Out
+                <LogOut className='w-4 h-4 ml-2' />
+            </DropdownMenuItem>
         </DropdownMenuContent>
     </DropdownMenu>
   )
