de77e4bfa17fed21f11b27adb535ce73090ea611
neeraj-gs <gsneeraj05@gmail.com>
2023-11-10 13:50:49 +0530

    ADded zod and infered int he form type and also added a resolver that is zod resolver and takes in the shape of our schema

--- coursegen-ai/src/components/CreateCourseForm.tsx
+++ coursegen-ai/src/components/CreateCourseForm.tsx
@@ -1,12 +1,28 @@
 'use client'
 import React from 'react'
+import { Form } from './ui/form'
+import { z } from 'zod'
+import { createChapterSchema } from '@/validators/course'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 type Props = {}
 
+type Input = z.infer<typeof createChapterSchema>//this si the shaoe of our form
+
 const CreateCourseForm = (props: Props) => {
+    const form = useForm<Input>({
+        resolver: zodResolver(createChapterSchema),
+        defaultValues:{
+            title:'',
+            units:['','','']
+        }
+    }) //tells react hhok formm tp have shape fo input type
   return (
     <div className='w-full'>
-        
+        <Form>
+            <form></form>
+        </Form>
     </div>
   )
 }
