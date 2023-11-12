230594c1b79a81512f4212456832cab6d75b04bb
neeraj-gs <gsneeraj05@gmail.com>
2023-11-12 10:06:44 +0530

    ADded teh end point need to add the gpt wrapper on it to generate teh chaptetrs

--- coursegen-ai/src/app/api/course/createChapters/route.ts
+++ coursegen-ai/src/app/api/course/createChapters/route.ts
@@ -2,7 +2,21 @@
 //once lets go ai then it will hit this endpoint adn it gfenerates some chapters
 
 import { NextResponse } from "next/server";
+import { createChapterSchema } from "@/validators/course";
+import { ZodError } from "zod";
 
 export async function POST(req:Request,res:Response){
-    return NextResponse.json({msg:'Hello World'})
+    //functionality of ai
+    try {
+        const body = await req.json();
+        const {title,units} = createChapterSchema.parse(body)
+
+        
+
+    } catch (error) {
+        if(error instanceof ZodError){ //it does not confirm to  schema we retunrded
+            return new NextResponse('invalid Body',{status:400})
+
+        }
+    }
 }
