de9e31bef8d72db476db0d62fd1e63d584042b52
neeraj-gs <gsneeraj05@gmail.com>
2023-12-21 17:58:52 +0530

    Added some basic checks for the course app and also included diffrent checks for it

--- coursegen-ai/src/app/create/[course_id]/page.tsx
+++ coursegen-ai/src/app/create/[course_id]/page.tsx
@@ -1,10 +1,37 @@
+import { getAuthSession } from '@/lib/auth';
+import { prisma } from '@/lib/db';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-type Props = {};
+type Props = {
+  params:{
+    course_id:string;
+  }
+};
 
-const CreateChapters = (props:Props) => {
+const CreateChapters = async({params:{course_id}}:Props) => {
+  const session = await getAuthSession();
+  if(!session?.user){
+    return redirect('/course')
+  }
+
+  const course = await prisma.course.findUnique({
+    where:{
+      id:course_id
+    },
+    include:{
+      units:{
+        include:{
+          chapters: true //prisma way of doing joins, get all teh joins and get hte tree strucutre of all courses , units and chapters
+        }
+      }
+    }
+  })
+  if(!course){
+    return redirect('/create')
+  }
   return (
-    <div className='pt-6'>CreateChapters</div>
+    <div className='pt-6'></div>
   )
 }
 
