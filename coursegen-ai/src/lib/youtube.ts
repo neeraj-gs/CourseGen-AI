22dc4387c6db77ccf7cac62cce86106bf2b6dfcf
neeraj-gs <gsneeraj05@gmail.com>
2023-12-22 19:04:19 +0530

    Added youtube api and added code to fetch a particular youtube api based on our search query that we got from our AI

---
+++ coursegen-ai/src/lib/youtube.ts
@@ -0,0 +1,26 @@
+export async function searchYouTube(searchQuery: string) {
+    searchQuery = searchQuery.replaceAll(" ", "+");
+    console.count("youtube search");
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&q=${searchQuery}&videoDuration=medium&videoEmbeddable=true&type=video&maxResults=5`,
+      {
+        method: "GET",
+      }
+    );
+    const json = await response.json();
+    if (!json) {
+      console.log("youtube fail");
+      return null;
+    }
+    if (json.items[0] == undefined) {
+      console.log("youtube fail");
+      return null;
+    }
+    return json.items[0].id.videoId; //if everything goes well we return the videoID
+  }
+
+  //return with the video id based on teh search query geenrated by AI 
+
+  export async function getTranscript(videoId:string){
+    
+  }
