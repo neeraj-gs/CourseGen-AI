c96cb35c119e58aafd01b8868cfdfffa33fa2923
neeraj-gs <gsneeraj05@gmail.com>
2023-12-24 19:13:31 +0530

    Added Webhook to make sure the specific user subscribed gets the subscription added

---
+++ coursegen-ai/src/app/api/webhook/route.ts
@@ -0,0 +1,45 @@
+import { prisma } from '@/lib/db'
+import { stripe } from '@/lib/stripe'
+import {headers} from 'next/headers'
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
+
+export async function POST(req:Request,res:Response){
+   const body = await req.text()
+   const signature = headers().get('Stripe-Signature')  as string
+   let event:Stripe.Event
+
+   try {
+    event = stripe.webhooks.constructEvent(
+        body,signature,process.env.STRIPE_WEBHOOK_SECRET as string
+    )
+   } catch (error) {
+    return new NextResponse("WebHook Error",{status:400})
+    
+   }
+
+   const session = event.data.object as Stripe.Checkout.Session
+
+   //new Subscription created
+   if(event.type === 'checkout.session.completed'){
+    const subscription = await stripe.subscriptions.retrieve(session.subscription as string)
+    if(!session?.metadata?.userId){
+        return new NextResponse("WebHook Error No User ID",{status:400})
+    }
+    await prisma.userSubscription.create({
+        data:{
+            userId:session.metadata.userId,
+            stripeSubscriptionId:subscription.id,
+            stripeCustomerId:subscription.customer as string,
+            stripePriceId:subscription.items.data[0].price.id,
+            stripeCurrentPeriodEnd:new Date(
+                subscription.current_period_end * 1000
+            )
+
+        }
+    })
+
+
+
+   }
+}
