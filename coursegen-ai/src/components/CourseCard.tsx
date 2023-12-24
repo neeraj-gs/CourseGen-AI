f16c06952ae1032d7b7ddfa1a26279319acbc5b2
neeraj-gs <gsneeraj05@gmail.com>
2023-12-24 12:10:52 +0530

    Added Course Page taht iapls all the created courses

--- coursegen-ai/src/components/CourseCard.tsx
+++ coursegen-ai/src/components/CourseCard.tsx
@@ -20,6 +20,15 @@ const CourseCard = async({c}: Props) => {
                     <span className="absolute px-2 py-1 text-white rounded-md bg-black/60 w-fit bottom-2 left-2 right-2">{c.name}</span>
                 </Link>
             </div>
+
+            <div className="p-4">
+                <h4 className="text-sm text-secondary-foreground/60">Units</h4>
+                <div className="space-y-1">{c.units.map((u,ui)=>{
+                    return(
+                        <Link className="block underline w-fit" key={u.id} href={`/courses/${c.id}/${ui}/0`}>{u.name}</Link>
+                    )
+                })}</div>
+            </div>
         </div>
     </>
   )
