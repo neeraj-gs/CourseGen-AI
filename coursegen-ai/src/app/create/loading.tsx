c04f8595a668b916eddc41b0e033d95c898fa9aa
neeraj-gs <gsneeraj05@gmail.com>
2023-12-24 22:58:23 +0530

    Added A Loader for each page for better UI

---
+++ coursegen-ai/src/app/create/loading.tsx
@@ -0,0 +1,15 @@
+//Shows a loading state , when nextjs renders a page
+import { Loader2 } from 'lucide-react'
+import React from 'react'
+
+type Props = {}
+
+const LoadingPage = (props: Props) => {
+  return (
+    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <Loader2 className='animate-spin w-10 h-10' />
+    </div>
+  )
+}
+
+export default LoadingPage
