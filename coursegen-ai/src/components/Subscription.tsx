f9d27d9b5cee1b57b677601a4b0c1374639bd229
neeraj-gs <gsneeraj05@gmail.com>
2023-12-24 19:04:06 +0530

    Added a frontend to connect upgrade to subscription and allow subscription for it

--- coursegen-ai/src/components/Subscription.tsx
+++ coursegen-ai/src/components/Subscription.tsx
@@ -4,16 +4,34 @@ import { useSession } from "next-auth/react"
 import { Progress } from "./ui/progress"
 import { Button } from "./ui/button"
 import { Zap } from "lucide-react"
+import { useState } from "react"
+import axios from "axios"
 
 type Props = {}
 
 const Subscription = (props: Props) => {
     const {data} = useSession()
+    const [loading,setLoading] = useState(false)
+    const handleSubscribe = async () => {
+        // /.hit stripe endpiint and redirect it there 
+        setLoading(true)
+        try {
+            const res = await axios.get('/api/stripe')
+            window.location.href = res.data.url
+            
+        } catch (error) {
+            console.log(error)
+            
+        }finally{
+            setLoading(false)
+        }
+
+    }
   return (
     <div className="flex flex-col items-center w-1/2 p-4 mx-auto mt-20 rounded-e-lg bg-secondary">
         {data?.user.credits} / 10 Free Generations
         <Progress value={data?.user.credits ? (data.user.credits/10)*100: 0} />
-        <Button className="mt-4 font-bold text-white trnasition bg-gradient-to-tr from-green-500 to-blue-500 hover:from-green-500  hover:to-blue-600">Upgrade to Pro<Zap className="fill-white ml-2" /> </Button>
+        <Button disabled={loading} onClick={handleSubscribe} className="mt-4 font-bold text-white trnasition bg-gradient-to-tr from-green-500 to-blue-500 hover:from-green-500  hover:to-blue-600">Upgrade to Pro<Zap className="fill-white ml-2" /> </Button>
     </div>
   )
 }
