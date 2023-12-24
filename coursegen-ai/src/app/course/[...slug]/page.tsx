cc0d9367728167f231045dca80e38c7d54d5b1cb
neeraj-gs <gsneeraj05@gmail.com>
2023-12-24 11:27:45 +0530

    Added Previous and Next Naviagation beteween chapters

--- coursegen-ai/src/app/course/[...slug]/page.tsx
+++ coursegen-ai/src/app/course/[...slug]/page.tsx
@@ -2,6 +2,8 @@
 import CourseSideBar from '@/components/CourseSideBar'
 import VideoSummary from '@/components/VideoSummary'
 import { prisma } from '@/lib/db'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import React from 'react'
 type Props={
@@ -40,6 +42,9 @@ const CoursePage = async({params:{slug}}:Props) => {
     return redirect('/courses')
   }
 
+  const nextChapter = unit.chapters[ci +1];
+  const prevChapter = unit.chapters[ci - 1];
+
 
   return (
     <div>
@@ -48,6 +53,37 @@ const CoursePage = async({params:{slug}}:Props) => {
         <div className='ml-[400px] px-8'>
           <div className='flex'>
             <VideoSummary chapter={chapter} ci={ci} unit={unit} ui={ui} />
+          </div>
+          <div className='flex-[1] h-[1px] mt-4 text-gray-500 bg-gray-500' />
+          <div className='flex pb-8'>
+            {prevChapter && (
+              <Link href={`/course/${course.id}/${ui}/${ci-1}`} className='flex mt-4 mr-auto w-fit'>
+                <div className='flex items-center'>
+                  <ChevronLeft className='w-6 h-6 mr-1' />
+                  <div className='flex flex-col items-start'>
+                    <span className='text-sm text-secondary-foreground/60'>
+                      Previous
+                    </span>
+                    <span className='text-xl font-bold'>{prevChapter.name}</span>
+                  </div>
+                </div>
+              </Link>
+            )}
+
+{nextChapter && (
+              <Link href={`/course/${course.id}/${ui}/${ci+1}`} className='flex mt-4 ml-auto w-fit'>
+                <div className='flex items-center'>
+                  
+                  <div className='flex flex-col items-start'>
+                    <span className='text-sm text-secondary-foreground/60'>
+                      Next
+                    </span>
+                    <span className='text-xl font-bold'>{nextChapter.name}</span>
+                    <ChevronRight className='w-6 h-6 mr-1' />
+                  </div>
+                </div>
+              </Link>
+            )}
           </div>
         </div>
       </div>
