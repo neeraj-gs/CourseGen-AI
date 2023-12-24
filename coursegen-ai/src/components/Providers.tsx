c9c36b063a179a5517152d4034013abea84e194e
neeraj-gs <gsneeraj05@gmail.com>
2023-12-24 18:26:19 +0530

    Added A Progress bar and also a pro subscriotion button for generations

--- coursegen-ai/src/components/Providers.tsx
+++ coursegen-ai/src/components/Providers.tsx
@@ -5,13 +5,18 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
+import { SessionProvider } from "next-auth/react"
 
 const queryClient = new QueryClient();
 //react query does a lot of caching ,[say we are on page fo app adn fetch same end point on diffrent appliactions page, react query understands it and fetches it and caches the previous end point and does  not need to sennd the request again]
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return  <QueryClientProvider client={queryClient }>
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>{children}</NextThemesProvider>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>
+      <SessionProvider>
+      {children}
+      </SessionProvider>
+      </NextThemesProvider>
   </QueryClientProvider>
   
 }
--- coursegen-ai/src/components/Subscription.tsx
+++ coursegen-ai/src/components/Subscription.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import { Progress } from "./ui/progress"
+import { Button } from "./ui/button"
+import { Zap } from "lucide-react"
 
 type Props = {}
 
 const Subscription = (props: Props) => {
+    const {data} = useSession()
   return (
-    <div>Subscription</div>
+    <div className="flex flex-col items-center w-1/2 p-4 mx-auto mt-4 rounded-e-lg bg-secondary">
+        {data?.user.credits} / 10 Free Generations
+        <Progress value={data?.user.credits ? (data.user.credits/10)*100: 0} />
+        <Button className="mt-4 font-bold text-white trnasition bg-gradient-to-tr from-green-500 to-blue-500 hover:from-green-500  hover:to-blue-600">Upgrade to Pro<Zap className="fill-white ml-2" /> </Button>
+    </div>
   )
 }
 
